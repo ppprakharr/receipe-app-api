c1d491d036512edfce4eceece509971fce8c6d4a
Prakhar Srivastava <prakharsrivastava@Prakhars-MacBook-Air.local>
2025-06-12 21:19:10 +0530

    made unit tests, db migrations and created user models

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin  # noqa
+"""
+django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (_('Important Dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            )
+        }),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
